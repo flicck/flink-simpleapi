e6d4c3b2b5b93899e2e8576bad12688a2582d010
HanWang1995 <2521117696@qq.com>
2020-05-21 20:41:58 +0800

    将StreamEnv下的第一级由方法改为属性类型

--- src/main/java/com/simpleapi/entry/StreamEnv.java
+++ src/main/java/com/simpleapi/entry/StreamEnv.java
@@ -4,28 +4,21 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
 public class StreamEnv {
     private StreamExecutionEnvironment env;
-    public StreamEnv(){
+    {
         this.env = StreamExecutionEnvironment.getExecutionEnvironment();
     }
-    public Source source(){
-        return new Source(env);
-    }
-    public From from(){
-        return new From(env);
-    }
+    public Source source = new Source(env);
+
+    public From from = new From(env);
+
     public StreamExecutionEnvironment getEnv(){
         return env;
     }
-    public Config config(){
-        return new Config(env);
-    }
-    public ExConfig exConfig(){
-        return new ExConfig(env);
-    }
-    public Register register(){
-        return new Register(env);
-    }
-    public CheckPoint checkPoint(){
-        return new CheckPoint(env);
-    }
+    public Config config = new Config(env);
+
+    public ExConfig exConfig = new ExConfig(env);
+
+    public Register register = new Register(env);
+
+    public CheckPoint checkPoint = new CheckPoint(env);
 }
